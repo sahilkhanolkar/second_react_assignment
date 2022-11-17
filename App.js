733ee66c96761f373a4465a369098e62d8c23917
Sahil Khanolkar <sahilkhanolkar@avegen-00189.lan>
2022-11-17 11:35:58 +0530

    base ready

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+import React,{useState} from 'react';
 
 export default function App() {
+  const [value, onChangeValue] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Lets Start This Project!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeValue}
+        value={value}
+        placeholder="Username"
+      />
+      <TouchableHighlight onPress={console.log("clicked")}>
+        <View style={styles.button}>
+          <Text>Login</Text>
+        </View>
+        </TouchableHighlight>
     </View>
   );
 }
@@ -16,5 +29,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
   },
 });
